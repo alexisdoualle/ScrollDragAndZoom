8ec4187188068d1bd5ef8a1bdbfe33243e3a8831
Alexis Doualle <adoualle@gmail.com>
2024-05-08 21:42:07 +0200

    Initial Commit

---
+++ ScrollDragAndZoom/ScrollDragAndZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollDragAndZoomApp.swift
+//  ScrollDragAndZoom
+//
+//  Created by Alexis Doualle on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrollDragAndZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
